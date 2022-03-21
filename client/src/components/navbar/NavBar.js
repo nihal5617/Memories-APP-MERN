4ffb0904721993eae9eb99e3256b0d275eeb1cb9
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-03-21 23:04:09 +0530

    Bugs Fixed

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -42,7 +42,7 @@ const NavBar = () => {
             <Toolbar className={classes.toolbar}>
                 {user?.result ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
                         <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
